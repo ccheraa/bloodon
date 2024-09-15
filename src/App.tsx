2082e6801b548f71555363351b2d8ed79175d761
Charafeddine Cheraa <ccheraa@gmail.com>
2024-09-15 12:51:19 +0300

    blank login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import './App.scss'
+import { LoginPage } from './pages/LoginPage'
 
 function App() {
   return (
-    <h1>APP</h1>
+    <>
+      <h1>APP</h1>
+      <LoginPage />
+    </>
   )
 }
 
